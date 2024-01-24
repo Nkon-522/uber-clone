975814d2283093069d35c55185581c79ada80f53
Nkon-522 <nkon.cs@gmail.com>
2024-01-23 21:51:18 -0500

    Add - Service Select component progress

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div id="Home" class="flex flex-col h-screen">
+      <div class="m-3 mb-auto">
+        <div class="grid grid-cols-2 gap-3 my-3">
+          <ServiceSelectLarge
+            text="Ride"
+            imageWidth="74"
+            image="ride"
+          />
+        </div>
+      </div>
+    </div>
   </main>
 </template>
+
+<script setup>
+  import ServiceSelectLarge from '../components/ServiceSelectLarge.vue';
+</script>
